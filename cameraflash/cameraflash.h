4818e111167ac52b833ffd4ab16cf26d3dbf9f20
Aleksey Gureiev <spyromus@noizeramp.com>
2012-11-29 09:02:26 +0200

    Initial Commit

---
+++ cameraflash/cameraflash.h
@@ -0,0 +1,13 @@
+//
+//  cameraflash.h
+//  cameraflash
+//
+//  Created by Aleksey Gureiev on 11/29/12.
+//  Copyright (c) 2012 Aleksey Gureiev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cameraflash : NSObject
+
+@end
